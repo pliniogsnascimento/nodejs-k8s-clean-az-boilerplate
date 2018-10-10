b50b5da6583b30043a25603b850065ec151f9e3a
Plinio Nascimento <plinio.gsnascimento@outlook.com>
2018-10-10 20:18:22 -0300

    Configurando rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,7 @@
 
-module.exports.health = require('./health.route')(app, config);
+module.exports.health = (app, config) => require('./health.route')(app, config);
 
 module.exports.configure = (app, config) => {
-    
     require('./jogo.route')(app, config.repo);
     require('./console.route')(app, config.repo);
 }
