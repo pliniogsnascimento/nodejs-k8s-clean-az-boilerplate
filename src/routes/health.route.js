572e38e8761bf4844e6fba4725231b152abaf3ae
Plinio Nascimento <plinio.gsnascimento@outlook.com>
2018-12-15 14:17:36 -0200

    Rodando esteira jenkins

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -2,6 +2,7 @@ module.exports = router => {
   router.get('/', (req, res) => {
     res.status(200).send('Products service');
   });
+  
   router.get('/health', (req, res) => {
     res.status(200).send('Service is alive!');
   });
