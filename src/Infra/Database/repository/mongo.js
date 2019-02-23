9446049adbdba4eeebc18b0ba53892a1ece337c3
Plinio <plinio@localhost.localdomain>
2019-02-23 13:37:45 -0300

    Adicionando configs de database

--- src/Infra/Database/repository/mongo.js
+++ src/Infra/Database/repository/mongo.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async ({ logger, config }) => {
     try {
-        const conn = await mongoose.connect(config.get('Database.dbConnectionString'),
-        { useNewUrlParser: true });
+        const conn = await mongoose.connect(config.get('Database.dbConnectionString'), { 
+                useNewUrlParser: true, 
+                connectTimeoutMS: config.get('Database.timeout'),
+                wTimeoutMS: config.get('Database.timeout')
+            });
+
         logger.info('Connected to mongo database');
+        
         return conn;
     }
     catch(err) {
         logger.error('Error connecting with the database');
-        connect();
+        logger.error(err);
     }
 }
 
