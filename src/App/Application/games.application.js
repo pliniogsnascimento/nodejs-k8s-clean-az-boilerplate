6acbc0284a1de38d1542e34a2533c5c88bc2fc12
Plinio Nascimento <plinio.gsnascimento@outlook.com>
2019-03-11 16:24:02 -0300

    First commit

--- src/App/Application/games.application.js
+++ src/App/Application/games.application.js
@@ -7,40 +7,10 @@ class GamesApplication {
     this.logger.debug('New instance: GamesApplication');
   }
 
-  createQueryObject(queryObject) {
-    // let query = {};
-
-    // for (let key in queryObject)
-    //   query[key] = queryObject[key];
-
-    // return query;
-  }
-
   async searchAllGames(params) {
     const games = await this.service.getAllGames(params);
 
     return games;
-
-    // let games;
-
-    // if (req.query.nome)
-    //   games = this.service.getGamesByName(req, res);
-
-    // else {
-      // const query = this.createQueryObject(req.query);
-
-      // if (query.limit) {
-      //   let limit = query.limit;
-      //   delete query.limit;
-      //   games = await this.service.getGamesWithPagination(query, limit);
-      // }
-      // else
-      //   games = await this.service.getAllGames(query, this.jogoModel);
-
-      // new serverResponse(games, res)
-      //   .addLink({ rel: 'self', href: req.path })
-      //   .ok();
-    // }
   }
 
   async saveGame(game) {
@@ -49,50 +19,6 @@ class GamesApplication {
     this.game.validate();
 
     return await this.service.saveGame(this.game.object);
-    // let game = req.body;
-    // const savedGame = await this.service.saveGame(game);
-    // new serverResponse(savedGame, res).created();
-  }
-
-  getGameById(id) {
-    // const id = { _id: req.params.id };
-
-    // const game = await this.service.getGameById(id, this.jogoModel)
-    // let resp = {
-    //   data: game,
-    //   links: [
-    //     {
-    //       rel: 'self',
-    //       href: req.path
-    //     }
-    //   ]
-    // }
-
-    // if (game.imagem) {
-    //   resp.links.push({
-    //     rel: 'imagem',
-    //     href: '/static/' + game.imagem
-    //   });
-    // }
-    // new serverResponse(game, res).ok();
-  }
-
-  updateGame(game) {
-    // let game = req.body;
-    // let id = { _id: req.params.id };
-
-    // const updatedGame = await this.service.updateGame(id, game);
-
-    // if (updatedGame !== game)
-    //   new serverResponse(jogo, res).ok();
-    // else
-    //   new serverResponse(err, res).notModified();
-  }
-
-  deleteGame(id) {
-    // let id = { _id: req.params.id };
-    // await this.service.deleteGame(id)
-    // new serverResponse(jogo, res).ok();
   }
 }
 
--- src/App/service/game.service.js
+++ src/App/service/game.service.js
@@ -8,15 +8,7 @@ class JogoService {
   }
 
   async getAllGames(query) { return await this.rep.getAllResources(query, this.gameModel) }
-  // getGamesByName = async name => this.rep.getAllResources({ nome: { '$regex': name, $options: 'i' } }, this.gameModel);
-  // getGameById = async id => this.rep.getResourceByID(id, this.jogoModel);
-
-  // //TODO: Concertar paginação
-  // getGamesWithPagination = async (query, limit) => this.rep.getResourceList(query, limit = 25, this.gameModel);
   async saveGame(game) { return await this.rep.postResource(new GameSchema(game)) }
-  // updateGame = async (id, game) => this.rep.updateResource(id, this.gameModel, game);
-  // deleteGame = async id => this.rep.deleteResource(id, this.gameModel);
-  
 }
 
 module.exports = JogoService;
